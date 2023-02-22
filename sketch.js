90556b2122a848531a59b856b34cf174d73771b9
LisaCDC <126083847+LisaCDC@users.noreply.github.com>
2023-02-22 16:02:34 +0100

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,19 @@
+var canvas;
+let image1;
+
+function setup() {
+  /*canvas = createCanvas(windowWidth, windowHeight * 10); //palette de l'endroit ou on peut dessiner
+  canvas.position(0, 0); // positionner sur la page le canvas
+  canvas.style ('z-index', '-10');
+  image1 =loadImage("donut.svg");*/
+}
+
+function draw() {
+//background(0);
+/*clear();
+circle(0,0,0);
+fill(2,0,0);
+strokeWeight(4);
+imageMode (CENTER)
+image(image1,mouseX,mouseY, 200, 0);*/
+}
